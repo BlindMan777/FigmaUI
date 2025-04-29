d8c58a0e90a9e8f41fa8f013bf78cc493de89dcb
Dmytro Kushmyruk <wmitya90@gmail.com>
2025-04-29 17:52:33 +0300

    TextField update is done

--- app/src/main/java/com/figma_ui/app/Prompt.kt
+++ app/src/main/java/com/figma_ui/app/Prompt.kt
@@ -4,20 +4,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -27,36 +30,37 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun Prompt() {
-    TextField(
+    var promptState by remember {
+        mutableStateOf("")
+    }
+
+    BasicTextField(
         modifier = Modifier
             .fillMaxWidth(),
-        minLines = 3,
-        value = "",
-        placeholder = {
-            Text(
-                text = "Type anything...",
-                color = Color.LightGray,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.SansSerif,
-                fontSize = 17.sp
-            )
+        value = promptState,
+        onValueChange = {newText ->
+            promptState = newText
         },
-        onValueChange = {},
         textStyle = TextStyle(
-            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            fontSize = 20.sp,
+            lineHeight = 20.sp
         ),
+        minLines = 4,
         singleLine = false,
-        colors = TextFieldDefaults.colors(
-            disabledContainerColor = Color.Transparent,
-            focusedContainerColor = Color.Transparent,
-            unfocusedContainerColor = Color.Transparent,
-            errorContainerColor = Color.Transparent,
-
-            focusedIndicatorColor = Color.Transparent,
-            errorIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent
-        )
+        decorationBox = { innerTextField ->
+            if(promptState.isEmpty()) {
+                Text(
+                    text = "Type anything...",
+                    color = Color.LightGray,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 17.sp
+                )
+            }
+            innerTextField()
+        },
+        cursorBrush = SolidColor(Color.Blue)
     )
     Row(
         modifier = Modifier
